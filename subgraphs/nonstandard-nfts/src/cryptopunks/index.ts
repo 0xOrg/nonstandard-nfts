eaf705d5189eba1065cff87e7171f4ba521b7956
Nick <nick@ethereumprice.org>
2021-06-23 12:08:05 +0100

    fix: bought punks were not being indexed correctly

--- subgraphs/nonstandard-nfts/src/cryptopunks/index.ts
+++ subgraphs/nonstandard-nfts/src/cryptopunks/index.ts
@@ -1,51 +1,51 @@
 import {
-	Account,
-	Transfer,
+  Account,
+  Transfer,
 } from '../../generated/schema'
 
 import {
-	CryptoPunk,
-	Assign              as AssignEvent,
-	PunkBidEntered      as PunkBidEnteredEvent,
-	PunkBidWithdrawn    as PunkBidWithdrawnEvent,
-	PunkBought          as PunkBoughtEvent,
-	PunkNoLongerForSale as PunkNoLongerForSaleEvent,
-	PunkOffered         as PunkOfferedEvent,
-	PunkTransfer        as PunkTransferEvent,
-	Transfer            as TransferEvent,
+  CryptoPunk,
+  Assign as AssignEvent,
+  PunkBidEntered as PunkBidEnteredEvent,
+  PunkBidWithdrawn as PunkBidWithdrawnEvent,
+  PunkBought as PunkBoughtEvent,
+  PunkNoLongerForSale as PunkNoLongerForSaleEvent,
+  PunkOffered as PunkOfferedEvent,
+  PunkTransfer as PunkTransferEvent,
+  Transfer as TransferEvent,
 } from '../../generated/CryptoPunks/CryptoPunk'
 
 import {
-	fetchRegistry,
-	fetchToken,
+  fetchRegistry,
+  fetchToken,
 } from '../utils'
 
 import {
-	constants,
-	events,
-	transactions,
+  constants,
+  events,
+  transactions,
 } from '@amxx/graphprotocol-utils'
 
 export function handleAssign(event: AssignEvent): void {
-	let registry = fetchRegistry(CryptoPunk.bind(event.address));
-	let token    = fetchToken(registry, event.params.punkIndex)
-	let from     = new Account(constants.ADDRESS_ZERO)
-	let to       = new Account(event.params.to.toHex())
+  let registry = fetchRegistry(CryptoPunk.bind(event.address));
+  let token = fetchToken(registry, event.params.punkIndex)
+  let from = new Account(constants.ADDRESS_ZERO)
+  let to = new Account(event.params.to.toHex())
 
-	token.owner = to.id
+  token.owner = to.id
 
-	registry.save()
-	token.save()
-	from.save()
-	to.save()
+  registry.save()
+  token.save()
+  from.save()
+  to.save()
 
-	let ev = new Transfer(events.id(event))
-	ev.transaction = transactions.log(event).id
-	ev.timestamp   = event.block.timestamp
-	ev.token       = token.id
-	ev.from        = from.id
-	ev.to          = to.id
-	ev.save()
+  let ev = new Transfer(events.id(event))
+  ev.transaction = transactions.log(event).id
+  ev.timestamp = event.block.timestamp
+  ev.token = token.id
+  ev.from = from.id
+  ev.to = to.id
+  ev.save()
 }
 
 export function handlePunkBidEntered(event: PunkBidEnteredEvent): void {
@@ -55,6 +55,25 @@ export function handlePunkBidWithdrawn(event: PunkBidWithdrawnEvent): void {
 }
 
 export function handlePunkBought(event: PunkBoughtEvent): void {
+  let registry = fetchRegistry(CryptoPunk.bind(event.address));
+  let token = fetchToken(registry, event.params.punkIndex)
+  let from = new Account(event.params.fromAddress.toHex())
+  let to = new Account(event.params.toAddress.toHex())
+
+  token.owner = to.id
+
+  registry.save()
+  token.save()
+  from.save()
+  to.save()
+
+  let ev = new Transfer(events.id(event))
+  ev.transaction = transactions.log(event).id
+  ev.timestamp = event.block.timestamp
+  ev.token = token.id
+  ev.from = from.id
+  ev.to = to.id
+  ev.save()
 }
 
 export function handlePunkNoLongerForSale(event: PunkNoLongerForSaleEvent): void {
@@ -64,25 +83,25 @@ export function handlePunkOffered(event: PunkOfferedEvent): void {
 }
 
 export function handlePunkTransfer(event: PunkTransferEvent): void {
-	let registry = fetchRegistry(CryptoPunk.bind(event.address));
-	let token    = fetchToken(registry, event.params.punkIndex)
-	let from     = new Account(event.params.from.toHex())
-	let to       = new Account(event.params.to.toHex())
+  let registry = fetchRegistry(CryptoPunk.bind(event.address));
+  let token = fetchToken(registry, event.params.punkIndex)
+  let from = new Account(event.params.from.toHex())
+  let to = new Account(event.params.to.toHex())
 
-	token.owner = to.id
+  token.owner = to.id
 
-	registry.save()
-	token.save()
-	from.save()
-	to.save()
+  registry.save()
+  token.save()
+  from.save()
+  to.save()
 
-	let ev = new Transfer(events.id(event))
-	ev.transaction = transactions.log(event).id
-	ev.timestamp   = event.block.timestamp
-	ev.token       = token.id
-	ev.from        = from.id
-	ev.to          = to.id
-	ev.save()
+  let ev = new Transfer(events.id(event))
+  ev.transaction = transactions.log(event).id
+  ev.timestamp = event.block.timestamp
+  ev.token = token.id
+  ev.from = from.id
+  ev.to = to.id
+  ev.save()
 }
 
 export function handleTransfer(event: TransferEvent): void {
